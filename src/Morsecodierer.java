febe24097a184281bfb241f86a255eb310b61821
levin2336 <levin-janssen@web.de>
2023-03-09 12:23:26 +0100

    encode Method

--- src/Morsecodierer.java
+++ src/Morsecodierer.java
@@ -1,15 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Morsecodierer {
+
+    BinaryTree morseTree;
     public Morsecodierer() {
-        BinaryTree morseTree = new BinaryTree<>(" ", new BinaryTree<>(),new BinaryTree<>());
+        morseTree = new BinaryTree<>(" ", new BinaryTree<>(),new BinaryTree<>());
     }
 
-    public String encodiereText(String pText){
+    public String encodiereText(String pText) throws IOException {
+        FileReader fr = new FileReader("morse.txt");
+        BufferedReader br = new BufferedReader(fr);
+        for (int i = 0; i < 29; i++) {
+            String zeile = br.readLine();
+            String[] parts = zeile.split(";");
+            zeichenEinfügen(parts[1],parts[2],morseTree);
+        }
+
         return "";
     }
     public String decodiereText(String pMorseCode){
         return "";
     }
     public void stelleBaumDar(){
-        BaumZeichner zeichner = new BaumZeichner(100,100,morseTree);
+        BaumZeichner zeichner = new BaumZeichner(900,900,morseTree);
     }
 }
