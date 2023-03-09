b3cec089993328314e6e1aeb2a47b4e393a3d986
henning.ruwwe <henning4454@gmail.com>
2023-03-09 12:02:52 +0100

    methods

--- src/Morsecodierer.java
+++ src/Morsecodierer.java
@@ -1,2 +1,15 @@
 public class Morsecodierer {
+    public Morsecodierer() {
+
+    }
+
+    public String encodiereText(String pText){
+        return "";
+    }
+    public String decodiereText(String pMorseCode){
+        return "";
+    }
+    public void stelleBaumDar(){
+
+    }
 }
