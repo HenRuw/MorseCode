77cd4478e1126dd3c9c6982e1642d944f5612ff1
levin2336 <levin-janssen@web.de>
2023-03-10 12:06:23 +0100

    fixed error in zeichenEinfügen

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Morsecodierer m  = new Morsecodierer();
-        m.setMorseTree(m.zeichenEinfügen('v', "...-", m.getMorseTree()));
-        System.out.println(m.getMorseTree().getContent());
-        //m.stelleBaumDar();
+        m.erstelleBaum();
+        m.stelleBaumDar();
     }
 }
--- src/Morsecodierer.java
+++ src/Morsecodierer.java
@@ -1,64 +1,64 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Morsecodierer {
 
     BinaryTree morseTree;
-    public Morsecodierer() {
-        morseTree = new BinaryTree<>(" ", new BinaryTree<>(),new BinaryTree<>());
-    }
 
-    public void setMorseTree(BinaryTree morseTree) {
-        this.morseTree = morseTree;
+    public Morsecodierer() {
+        morseTree = new BinaryTree<>(" ", new BinaryTree<>(), new BinaryTree<>());
     }
 
     public BinaryTree getMorseTree() {
         return morseTree;
     }
 
-    public String encodiereText(String pText){
+    public void setMorseTree(BinaryTree morseTree) {
+        this.morseTree = morseTree;
+    }
+
+    public String encodiereText(String pText) {
 
         return "";
     }
-    public String decodiereText(String pMorseCode){
+
+    public String decodiereText(String pMorseCode) {
         return "";
     }
-    public void stelleBaumDar(){
-        BaumZeichner zeichner = new BaumZeichner(900,900,morseTree);
+
+    public void stelleBaumDar() {
+        BaumZeichner zeichner = new BaumZeichner(900, 900, morseTree);
     }
+
     public void erstelleBaum() throws IOException {
         FileReader fr = new FileReader("src/morse.txt");
         BufferedReader br = new BufferedReader(fr);
         for (int i = 0; i < 29; i++) {
             String zeile = br.readLine();
-            String[] parts = zeile.split(";"); //TODO funktioniert nicht, gibt keinen Buchstaben in part 1
-            System.out.println(parts[1]);
-            System.out.println(parts[1].charAt(0) + " parts 1" + parts[1]);
-            zeichenEinfügen(parts[1].charAt(0),parts[1],morseTree);
+            String[] parts = zeile.split(";");
+            zeichenEinfügen(parts[0].charAt(0), parts[1], morseTree);
         }
     }
-    public BinaryTree zeichenEinfügen(char z, String code, BinaryTree baum){
+
+    public BinaryTree zeichenEinfügen(char z, String code, BinaryTree baum) {
         String newCode = "";
-        if (baum.isEmpty()){
-            baum =  new BinaryTree<>(" ", new BinaryTree<>(),new BinaryTree<>());
+        if (baum.isEmpty()) {
+            baum = new BinaryTree<>(z, new BinaryTree<>(), new BinaryTree<>());
         }
-        if (code == ""){
-            baum.setContent("");
+        if (code.equals("")) {
+            baum.setContent(z);
         } else {
             if (code.charAt(0) == '.') {
                 for (int i = 1; i < code.length(); i++) {
                     newCode += code.charAt(i);
                 }
-                System.out.println("code: " + code + "newCode: " + newCode + "Buchstabe: " + z);
-                baum.setLeftTree(zeichenEinfügen(z, newCode, baum));
+                baum.setLeftTree(zeichenEinfügen(z, newCode, baum.getLeftTree()));
             } else {
                 for (int i = 1; i < code.length(); i++) {
-                    newCode += code.charAt(i+1);
+                    newCode += code.charAt(i);
                 }
-                System.out.println("code: " + code + "newCode: " + newCode + "Buchstabe: " + z);
-                baum.setRightTree(zeichenEinfügen(z, newCode, baum));
+                baum.setRightTree(zeichenEinfügen(z, newCode, baum.getRightTree()));
             }
         }
         return baum;
